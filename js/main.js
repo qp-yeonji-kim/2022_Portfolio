1c48cd6657f7fd7e913da9b56dbf9310998636c6
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2022-01-04 17:17:49 +0900

    each문 정리

--- js/main.js
+++ js/main.js
@@ -70,10 +70,7 @@ $(function () {
       })
   });
 
-
   $(window).scroll(function () {
-    console.log($(window).scrollTop());
-    console.log($('.first_int').offset().top);
     if ($(window).scrollTop() >= $('.first_int').offset().top) {
       $('.pagination').css({
         'display': 'flex'
@@ -90,120 +87,112 @@ $(function () {
     }
   })
 
-/* 
-  $('.sc_project .title_area > * span').each(function (index, item) {
-    let triggerEl = $(this).parents('section');
+  $('.summary').each(function (index, item) {
+    let triggerEl = $(this);
+    let l_cnt01Wrap = $(this).find('.cnt_area_left .cnt_wrap');
+    let l_cnt01 = $(this).find('.cnt_area_left .cnt_wrap .cnt01');
+    let r_cnt02 = $(this).find('.cnt_area_right .cnt02 .fade');
+    let l_cnt03Cover01 = $(this).find('.cnt_area_left .cnt03 .img_cover01');
+    let l_cnt03Cover02 = $(this).find('.cnt_area_left .cnt03 .img_cover02');
+    let l_cnt03Wrap = $(this).find('.cnt_area_left .cnt03 .img_wrap');
+    let r_cnt03 = $(this).find('.cnt_area_right .cnt03');
+    let r_cnt03Cover01 = $(this).find('.cnt_area_right .cnt03 .img_cover01');
+    let l_cnt04 = $(this).find('.cnt_area_left .cnt04');
+    let r_cnt03Cover02 = $(this).find('.cnt_area_right .cnt03 .img_cover02');
+    let r_cnt04Txt = $(this).find('.cnt_area_right .cnt04 .fade')
+    let r_cnt04Li = $(this).find('.cnt_area_right .cnt04 li')
+    let r_cnt04 = $(this).find('.cnt_area_right .cnt04');
 
-    let tl = gsap.timeline({
+    gsap.to(l_cnt01Wrap, {
+      scrollTrigger: {
+        trigger: triggerEl,
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: 1,
+        duration: 1,
+        markers: true,
+      },
+      width: '100%',
+    })
+    gsap.to(l_cnt01, {
+      scrollTrigger: {
+        trigger: triggerEl,
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: .1,
+      },
+      opacity: .9,
+    })
+
+    let firstPjSum = gsap.timeline({
       scrollTrigger: {
         trigger: triggerEl,
         start: 'top top',
-        end: '+=2000',
+        end: '+=12000',
         scrub: 1,
         pin: true,
       }
-    });
+    })
 
-    tl.from(item, {
+    firstPjSum
+      .from(r_cnt02, {
         yPercent: 200,
         opacity: 0,
+        stagger: 0.1,
       })
-      .to(item, {
+      .to(r_cnt02, {
         immediateRender: false,
         yPercent: -200,
+        opacity: 0,
         delay: .3,
       })
-  });
-*/
-
-  gsap.to('.first_summary .cnt_area_left .cnt_wrap', {
-    scrollTrigger: {
-      trigger: $('.first_summary'),
-      start: 'top bottom',
-      end: 'bottom top',
-      scrub: 1,
-    },
-    width: '100%',
+      .to(l_cnt03Cover01, {
+        width: '50%',
+      }, 'cnt03-1')
+      .to(l_cnt03Cover02, {
+        height: '50%',
+      }, 'cnt03-1')
+      .to(r_cnt03, {
+        display: 'block',
+      }, 'cnt03-1')
+      .to(r_cnt03Cover01, {
+        width: 0,
+      }, 'cnt03-1')
+      .from(l_cnt03Wrap, {
+        clipPath: 'inset(50%)',
+      }, 'cnt03-1')
+      .to(l_cnt03Wrap, {
+        clipPath: 'inset(50%)',
+      }, 'cnt03-2')
+      .from(l_cnt04, {
+        clipPath: 'inset(50%)',
+      }, 'cnt03-2')
+      .to(r_cnt03Cover02, {
+        height: '50%',
+      }, 'cnt03-2')
+      .to(r_cnt04, {
+        display: 'flex',
+        delay: .2,
+      }, 'cnt03-2')
+      .from(r_cnt04Li, {
+        opacity: 0,
+      }, 'cnt03-3')
+      .from(r_cnt04Txt, {
+        yPercent: 200,
+        opacity: 0,
+        delay: .2,
+      }, 'cnt03-3')
+      .to(r_cnt04Txt, {
+        immediateRender: false,
+        yPercent: -200,
+        opacity: 0,
+        delay: .2,
+      }, 'cnt04-1')
+      .to(r_cnt04Li, {
+        opacity: 0,
+      }, 'cnt04-1')
+      .to(r_cnt04, {
+        display: 'none',
+      }, 'cnt04-2')
   })
-  gsap.to('.first_summary .cnt_area_left .cnt_wrap .cnt01', {
-    scrollTrigger: {
-      trigger: $('.first_summary'),
-      start: 'top bottom',
-      end: 'bottom top',
-      scrub: .1,
-    },
-    opacity: .9,
-  })
-
-  let firstPjSum = gsap.timeline({
-    scrollTrigger: {
-      trigger: $('.first_summary'),
-      start: 'top top',
-      end: '+=8000',
-      scrub: 1,
-      pin: true,
-    }
-  })
-  firstPjSum
-    .from('.first_summary .cnt_area_right .cnt02 .fade', {
-      yPercent: 200,
-      opacity: 0,
-      stagger: 0.1,
-    })
-    .to('.first_summary .cnt_area_right .cnt02 .fade', {
-      immediateRender: false,
-      yPercent: -200,
-      opacity: 0,
-      delay: .3,
-    })
-    .to('.first_summary .cnt_area_right .cnt03', {
-      display: 'block',
-    })
-    .to('.first_summary .cnt_area_right .cnt03 .img_cover01', {
-      width: 0,
-    }, 'cnt03-1')
-    .to('.first_summary .cnt_area_right .cnt03 .img_cover02', {
-      height: '50%',
-    }, 'cnt03-2')
-    .to('.first_summary .cnt_area_left .cnt03 .img_cover01', {
-      width: '50%',
-    }, 'cnt03-1')
-    .to('.first_summary .cnt_area_left .cnt03 .img_cover02', {
-      height: '50%',
-    }, 'cnt03-1')
-    .from('.first_summary .cnt_area_left .cnt03 .img_wrap', {
-      clipPath: 'inset(50%)',
-    }, 'cnt03-1')
-    .to('.first_summary .cnt_area_left .cnt03 .img_wrap', {
-      clipPath: 'inset(50%)',
-    }, 'cnt03-2')
-    .from('.first_summary .cnt_area_left .cnt04', {
-      clipPath: 'inset(50%)',
-    }, 'cnt03-2')
-    .to('.first_summary .cnt_area_right .cnt04', {
-      display: 'flex',
-      delay: .2,
-    }, 'cnt03-2')
-    .from('.first_summary .cnt_area_right .cnt04 li', {
-      borderBottom: '1px solid transparent',
-      delay: .2,
-    }, 'cnt03-2')
-    .from('.first_summary .cnt_area_right .cnt04 .fade', {
-      yPercent: 200,
-      opacity: 0,
-      delay: .2,
-    }, 'cnt03-2')
-    .to('.first_summary .cnt_area_right .cnt04 .fade', {
-      immediateRender: false,
-      yPercent: -200,
-      opacity: 0,
-      delay: .2,
-    }, 'cnt04')
-    .to('.first_summary .cnt_area_right .cnt04', {
-      display: 'none',
-    }, 'cnt04')
-    .to('.first_summary .cnt_area_right .cnt04 li', {
-      borderBottom: '1px solid transparent',
-      delay: .2,
-    }, 'cnt04')
 })
